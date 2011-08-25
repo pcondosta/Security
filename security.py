2e9fe56ec6f46a776215ee8aac2e1c18bf23b9ac
Peter Condosta <pete@condosta.com>
2011-08-25 10:57:21 -0400

    changed input handler

--- security.py
+++ security.py
@@ -19,21 +19,6 @@ notifo = Notifo(user="pcondosta", secret="xc53e5cdc470b9adb5165adcd4aeb597549136
 db=MySQLdb.connect(host="localhost",user="pete",passwd="secnet",db="security")
 c=db.cursor()
 
-def getSensorInfo( sensorLoc, sensorVal):
-        msgState = "0"
-        if sensorVal > 950:
-                msgState = "Closed"
-        if sensorVal == 0:
-                msgState = "Opened"
-
-        if sensorLoc == 0:
-                sensor = "BSDR"
-        if sensorLoc == 1:
-                sensor = "BSLR"
-        if sensorLoc == 2:
-                sensor = "FD"
-        return sensor,msgState
-
 try:
         interfaceKit = InterfaceKit()
 except RuntimeError as e:
@@ -45,26 +30,40 @@ print ("%d attached!" % (interfaceKit.getSerialNum()))
 
 print ("Number of Sensor Inputs: %i" % (interfaceKit.getSensorCount()))
 
-def interfaceKitSensorChanged(e):
+def getSensorInfo( sensorLoc, sensorVal):
+        msgState = "0"
+        if sensorVal:
+                msgState = "Closed"
+        else:
+                msgState = "Opened"
+
+        if sensorLoc == 0:
+                sensor = "BSDR"
+        if sensorLoc == 1:
+                sensor = "BSLR"
+        if sensorLoc == 2:
+                sensor = "FD"
+        return sensor,msgState
+
+def interfaceKitInputChanged(e):
         sensor,state = getSensorInfo(e.index, e.value)
-        if state != "0":
-                print ("%s: %s, %i, %s" % (sensor, state, e.value, getTime()))
-                c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES (%s, %s, %s)""", (sensor, state, getTime()))
-                msgState = state + " at " + getTime()
-                print msgState
-                notifo.send_notification(label="Security", title=sensor, msg=msgState)
-                time.sleep(7)
+        print ("%s: %s, %i, %s" % (sensor, state, e.value, getTime()))
+#        c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES (%s, %s, %s)""", (sensor, state, getTime()))
+        msgState = state + " at " + getTime()
+        print msgState
+#        notifo.send_notification(label="Security", title=sensor, msg=msgState)
+#        time.sleep(7)
 try:
-        interfaceKit.setOnSensorChangeHandler(interfaceKitSensorChanged)
+        interfaceKit.setOnInputChangeHandler(interfaceKitInputChanged)
         print "Started at " + getTime()
-        c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES ('Security System', 'ON', %s)""", (getTime()))
-        notifo.send_notification(label="Security", title="Started", msg="Security is on")
+#        c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES ('Security System', 'ON', %s)""", (getTime()))
+#        notifo.send_notification(label="Security", title="Started", msg="Security is on")
 except PhidgetException as e:
         print ("Phidget Exception %i: %s" % (e.code, e.details))
 
 chr = sys.stdin.read(1)
 
-c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES ('Security System', 'OFF', %s)""", (getTime()))
+#c.execute("""INSERT INTO sensors_audit (sensor, state, time) VALUES ('Security System', 'OFF', %s)""", (getTime()))
 
 interfaceKit.closePhidget()
 c.close()
